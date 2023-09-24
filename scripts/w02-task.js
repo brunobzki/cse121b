4f36dc7395511ba01df7c637fd410ee79e087c00
brunobzki <bpanzacchi@gmail.com>
2023-09-23 21:14:41 -0300

    Week 02 ready!!

---
+++ scripts/w02-task.js
@@ -0,0 +1,40 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+
+let fullName = "Bruno Panzacchi";
+let currentYear = "2023";
+let profilePicture = "images/bruno.png";
+
+
+/* Step 3 - Element Variables */
+
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('img');
+
+
+/* Step 4 - Adding Content */
+
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.innerHTML = `<strong>${currentYear}</strong>`;
+imageElement.setAttribute('src', profilePicture);
+
+
+
+/* Step 5 - Array */
+
+let favoriteFood = ["Hamburger"," Asado"," Ravioles"," Gnocchi"," Bondiola"," Milanesa"]
+foodElement.innerHTML = `<br>${favoriteFood}`;
+
+favoriteFood.push(' Soup')
+foodElement.innerHTML += `<br>${favoriteFood}`;
+
+favoriteFood.shift()
+foodElement.innerHTML += `<br>${favoriteFood}`;
+
+favoriteFood.pop()
+foodElement.innerHTML += `<br>${favoriteFood}`;
