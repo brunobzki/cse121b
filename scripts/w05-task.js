be51b207c22ee99bc8d3f2d5fb684f171bb68046
brunobzki <bpanzacchi@gmail.com>
2023-10-14 14:08:22 -0300

    Some

---
+++ scripts/w05-task.js
@@ -0,0 +1,25 @@
+/* W05: Programming Tasks */
+
+/* Declare and initialize global variables */
+const templesElement = document.getElementById("temples");
+templeList=[]
+/* async displayTemples Function */
+
+const displayTemples= (temples)=> {
+    
+}
+
+
+/* async getTemples Function using fetch()*/
+
+
+/* reset Function */
+
+
+/* sortBy Function */
+
+
+
+getTemples();
+
+/* Event Listener */
